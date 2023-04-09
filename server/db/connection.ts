581ebf736af91ec00c163cf758a6f45fcb4827a9
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-04-09 17:09:32 -0400

    Setup server to use postgres
    Co-authored-by: Anthony Tornetta <cornchipgonecodin@gmail.com>

--- server/db/connection.ts
+++ server/db/connection.ts
@@ -1,7 +1,7 @@
 // https://www.mongodb.com/compatibility/using-typescript-with-mongodb-tutorial
 
 // External Dependencies
-
+// const Sequelize = require('sequelize');
 import dotenv from "dotenv";
 import postgres from "postgres";
 
@@ -19,71 +19,3 @@ const sql = postgres({
 });
 
 export default sql;
-//validation to our existing clients
-/*
- await db.command({
-    "collMod": process.env.USER_CLIENT_NAME,
-    "validator": {
-        $jsonSchema: {
-            bsonType: "object",
-            required: [
-                "name", 
-                "company", 
-                "email", 
-                "phone", 
-                "street", 
-                "city",                
-                "state", 
-                "zip", 
-                "login", 
-                "password" 
-                ],
-            additionalProperties: true,
-            properties: {
-            _id: {},
-            name: {
-                bsonType: "string",
-                description: "'name' is required and is a string"
-            },
-            company: {
-                bsonType: "string",
-                description: "'company' is required and is a string"
-            },
-            email: {
-                bsonType: "string",
-                description: "'email' is required and is a string"
-            },
-            phone: {
-                bsonType: "string",
-                description: "'phone' is required and is a string"
-            },
-            street: {
-                bsonType: "string",
-                description: "'steet' is required and is a string"
-            },
-            street2: {
-                bsonType: "string",
-                description: "'steet2' is optional and is a string"
-            },
-            city: {
-                bsonType: "string",
-                description: "'email' is required and is a string"
-            },
-            category: {
-                bsonType: "string",
-                description: "'city' is required and is a string"
-            },
-            state: {
-                bsonType: "string",
-                description: "'state' is required and is a string"
-            },
-            zip: {
-                bsonType: "number",
-                description: "'zip' is required and is a string"
-            }
-            }
-        }
-     }
-     
-});
-*/
