036b9ea007aba785c195fe2c524b63a4c01a2b8f
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-06-25 16:23:31 -0400

    Linked blog up to main page

--- server/routes/blog/index.ts
+++ server/routes/blog/index.ts
@@ -1,19 +1,40 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import sql from '../../db/connection';
 import { marked } from 'marked';
 import { readFile } from 'fs';
+import ejs from 'ejs';
 
 const router = express.Router();
 
 router.get('/:blogId', async (req, res, next) => {
-    console.log(req.params);
     const blogId = req.params.blogId;
 
+    if (isNaN(Number(blogId))) {
+        res.status(404).redirect('/');
+        return;
+    }
+
+    try {
+        const filePaths = await
+            sql<{ file_path: string }[]>`SELECT file_path FROM Posts WHERE post_id = ${blogId};`;
+
+        renderAndSend(filePaths, req, res);
+    }
+    catch (_) {
+        res.status(404).redirect('/');
+    }
+});
+
+router.get('/', async (req, res, next) => {
     const filePaths = await
-        sql<{ file_path: string }[]>`SELECT file_path FROM Posts WHERE post_id = ${blogId};`;
+        sql<DatabaseResult>`SELECT file_path FROM Posts ORDER BY created_date DESC LIMIT 1;`;
 
-    console.log(filePaths);
+    await renderAndSend(filePaths, req, res);
+});
 
+type DatabaseResult = { file_path: string }[];
+
+async function renderAndSend(filePaths: DatabaseResult, req: Request, res: Response) {
     if (filePaths.length !== 1) {
         res.status(404).redirect('/');
         return;
@@ -21,19 +42,37 @@ router.get('/:blogId', async (req, res, next) => {
 
     const filePath = filePaths[0].file_path;
 
-    readFile(`./templated/blog/${filePath}`, (err, data) => {
+    // Get all posts to display on the right navigation menu
+    const allPosts = await sql<{
+        post_id: number,
+        title: string
+    }[]>`SELECT post_id, title FROM Posts ORDER BY created_date DESC`;
+
+    readFile(`./blog/${filePath}`, (err, data) => {
         if (err) {
             res.status(404).redirect('/');
 
             console.error(err);
         }
 
-        const parsedHtml = marked.parse(data.toLocaleString(), {
-            mangle: false
+        const blogHtml = marked.parse(data.toLocaleString(), {
+            mangle: false,
+            headerIds: false,
         });
 
-        res.status(200).type('html').send(parsedHtml);
+        ejs.renderFile("templated/blog.ejs", {
+            user: req.oidc.user,
+            blogHtml,
+            allPosts
+        }, function (err, compiled) {
+            if (err) {
+                res.status(404).send("404 not found ;(");
+            } else {
+                res.status(200).send(compiled);
+            }
+        });
+
     });
-});
+}
 
 export default router;
