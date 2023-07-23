9db180c2ce9920b7518431511c48b3c48991c165
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2023-07-23 18:57:04 -0400

    add article script for db

---
+++ server/db/scripts/articles/all-articles.sql
@@ -0,0 +1,19 @@
+SELECT * FROM posts;
+
+INSERT INTO posts (title, file_path, created_date, author_id) 
+VALUES ('How Can You Keep Up with Consumer Behavior?', '2023/june/ConsumerBehaviorChanging.md', '2023-06-25 19:52:55.255415', 1);
+
+INSERT INTO posts (title, file_path, created_date, author_id) 
+VALUES ('What Is An XSS Attack?', '2023/july/XSSAttack.md', '2023-07-02 14:02:32.770638', 1);
+
+INSERT INTO posts (title, file_path, created_date, author_id) 
+VALUES ('Building a Website with the Right Goals', '2023/july/Website-Goals.md', '2023-07-19 20:32:00.412726', 1);
+
+INSERT INTO posts (title, file_path, created_date, author_id) 
+VALUES ('What does it mean to make a website more interactive?', '2023/july/Interactive-Website.md', '2023-07-22 09:49:18.440166', 1);
+
+INSERT INTO posts (title, file_path, created_date, author_id) 
+VALUES ('What is the Importance of a Database?', '2023/july/database-importance.md', '2023-07-22 11:43:05.479151', 1);
+
+INSERT INTO posts (title, file_path, created_date, author_id) 
+VALUES ('Do Colors Matter in a Website Design?', '2023/july/color-importance.md', "2023-07-23 07:10:26.617006", 1);
