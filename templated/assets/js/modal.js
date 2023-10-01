37a7f77dd2845aadec47d2db0f150bd1466bf565
AnthonyTornetta <cornchipgonecodin@gmail.com>
2023-10-01 18:21:11 -0400

    Fixed contact page

--- templated/assets/js/modal.js
+++ templated/assets/js/modal.js
@@ -1,5 +1,5 @@
-var modal = {
-    alert(message) {
+window.modal = {
+    alert(title, message, callback) {
         const modalTemplate = document.getElementById('modal-base').content;
 
         const modal = modalTemplate.cloneNode(true);
@@ -7,9 +7,13 @@ var modal = {
         container.append(modal);
 
         container.querySelector('.message').textContent = message;
+        container.querySelector('.title').textContent = title;
         const closeBtn = container.querySelector('.close');
         closeBtn.onclick = () => {
             container.remove();
+            if (callback) {
+                callback();
+            }
         }
         document.body.append(container);
 
